1775971519686a601af9822afc49838c16404d59
Nikola Trubitsyn <nikola.trubitsyn@gmail.com>
2019-02-17 20:54:10 +0300

    Remove license field

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     author='Nikola Trubitsyn',
     author_email='nikola.trubitsyn@gmail.com',
     url='https://github.com/trubitsyn/bmstu-schedule-diff',
-    license='AGPLv3+',
     python_requires='>=3.6',
     long_description_content_type='text/markdown',
     description='Display BMSTU schedule diff',
