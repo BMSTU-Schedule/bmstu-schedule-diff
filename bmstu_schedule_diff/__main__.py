e695c79c48498080d48b2cb3fc9e29b9e0a68a77
Nikola Trubitsyn <nikola.trubitsyn@gmail.com>
2019-02-24 17:41:26 +0300

    Beautify output

--- bmstu_schedule_diff/__main__.py
+++ bmstu_schedule_diff/__main__.py
@@ -14,13 +14,13 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import argparse
-import re
-from typing import Tuple, List, Dict
+from typing import List, Dict
 
 import requests
-from bmstu_schedule import Subject, Lesson
+from bmstu_schedule import Lesson
 
+from .display import Display
+from .args import argparser
 from .flag import Flag
 from .parser import get_schedule
 from .patch import patch_bmstu_schedule
@@ -28,84 +28,35 @@ from .schedule import weekday_schedule
 
 
 def main():
+    display = Display()
     patch_bmstu_schedule()
     args = vars(argparser().parse_args())
     groups = set(map(lambda group: group.upper(), args['groups']))
     if len(groups) != 2:
-        print("Please specify two different groups.")
+        display.notify_invalid_args()
         return
 
     schedules: Dict[str, List[Lesson]] = {}
     for group in groups:
-        print(f"Downloading {group} schedule...")
+        display.notify_downloading(group)
         try:
             schedule = get_schedule(group)
         except requests.exceptions.ConnectionError:
-            print(f"Failed to download {group} schedule: network is unreachable.")
+            display.notify_failed_network(group)
             return
 
         schedules[group] = schedule
 
     schedules_for_diff = [weekday_schedule(group, schedules[group]) for group in schedules.keys()]
     flags = Flag.SAME_BUILDING | Flag.NEARBY_FLOOR | Flag.NEARLY_SAME_TIME
-    print("Looking for close lessons...\n")
+    display.notify_searching(flags)
 
     schedule1 = schedules_for_diff[0]
     schedule2 = schedules_for_diff[1]
     diff = schedule1.diff(schedule2, flags)
 
-    print_results(diff)
-
-
-WEEKDAYS = (
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-)
-
-
-def print_results(results: Dict[int, List[Tuple[Subject, Subject]]]):
-    if all(not results[key] for key in results.keys()):
-        print("Nothing found.")
-        return
-
-    for weekday in results.keys():
-        if not results[weekday]:
-            continue
-        print(f"{WEEKDAYS[weekday]}:")
-        for pair in results[weekday]:
-            print(f"{weeks_interval(pair[0].weeks_interval)} ({pretty_print_subject(pair[0])}, {pretty_print_subject(pair[1])})")
-        print()
-
-
-def weeks_interval(weeks_interval):
-    if weeks_interval == 1:
-        return "ЧС"
-    return "ЗН"
-
-
-def pretty_print_subject(subject):
-    def format_time(time):
-        return ":".join(re.findall('..', time)[:-1])
-
-    return f"{format_time(subject.start_time)}-{format_time(subject.end_time)} {subject.name} {subject.auditorium}"
-
-
-def argparser():
-    parser = argparse.ArgumentParser(
-        description='Display BMSTU schedule diff',
-        add_help=True
-    )
-    parser.add_argument(
-        'groups',
-        type=str,
-        nargs='*',
-        help='Group identifiers'
-    )
-    return parser
+    display.print_results(schedule1.group, schedule2.group, diff)
+    display.destroy()
 
 
 if __name__ == '__main__':
---
+++ bmstu_schedule_diff/display.py
@@ -0,0 +1,109 @@
+# bmstu-schedule-diff
+# Copyright (C) 2019 Nikola Trubitsyn
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import re
+from typing import Tuple, List, Dict
+
+import colorama
+from bmstu_schedule import Subject
+from colorama import Fore, Style
+
+
+class Display(object):
+
+    def __init__(self):
+        colorama.init(autoreset=True)
+
+    def destroy(self):
+        colorama.deinit()
+
+    def info(self, msg: str):
+        print(msg)
+
+    def error(self, msg: str):
+        print(Fore.RED + msg)
+
+    def notify_invalid_args(self):
+        self.error("Please specify two different groups.")
+
+    def notify_downloading(self, group):
+        self.info(f"Downloading {group} schedule...")
+
+    def notify_failed_network(self, group):
+        self.error(f"Failed to download {group} schedule: network is unreachable.")
+
+    def notify_searching(self, flags):
+        self.info("Looking for close lessons...\n")
+
+    def print_results(self, group1, group2, results: Dict[int, List[Tuple[Subject, Subject]]]):
+        if all(not results[key] for key in results.keys()):
+            self.error("Nothing found.")
+            return
+
+        max_length_1 = 0
+        max_length_2 = 0
+        for weekday in results.keys():
+            if not results[weekday]:
+                continue
+            for pair in results[weekday]:
+                subject1 = pretty_print_subject(pair[0])
+                subject2 = pretty_print_subject(pair[1])
+                if len(subject1) > max_length_1:
+                    max_length_1 = len(subject1)
+                if len(subject2) > max_length_2:
+                    max_length_2 = len(subject2)
+
+        print(group1.center(max_length_1), group2.center(max_length_2))
+
+        for weekday in results.keys():
+            if not results[weekday]:
+                continue
+            print(Style.BRIGHT + f"{WEEKDAYS[weekday]}:")
+
+            for pair in results[weekday]:
+                interval = weeks_interval(pair[0].weeks_interval)
+                subject1 = pretty_print_subject(pair[0])
+                subject2 = pretty_print_subject(pair[1])
+                print(interval, color_subject(subject1.ljust(max_length_1)), color_subject(subject2.ljust(max_length_2)))
+
+        print()
+
+
+def pretty_print_subject(subject):
+    def format_time(time):
+        return ":".join(re.findall('..', time)[:-1])
+
+    return f"{format_time(subject.start_time)}-{format_time(subject.end_time)} {subject.name} {subject.auditorium}"
+
+
+def color_subject(subject: str):
+    return f"{colorama.Back.LIGHTCYAN_EX} {subject} {colorama.Back.RESET}"
+
+
+def weeks_interval(weeks_interval):
+    if weeks_interval == 1:
+        return Fore.LIGHTWHITE_EX + colorama.Back.GREEN + " ЧС " + colorama.Fore.RESET + colorama.Back.RESET
+    return Fore.LIGHTWHITE_EX + colorama.Back.BLUE + " ЗН " + colorama.Fore.RESET + colorama.Back.RESET
+
+
+WEEKDAYS = (
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+)
