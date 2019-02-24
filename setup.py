e752929690ac0e3c18086e82e178d30c76612b1c
Nikola Trubitsyn <nikola.trubitsyn@gmail.com>
2019-02-24 18:42:39 +0300

    Version 1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bmstu-schedule-diff',
-    version='1.1.0',
+    version='1.2.0',
     author='Nikola Trubitsyn',
     author_email='nikola.trubitsyn@gmail.com',
     url='https://github.com/trubitsyn/bmstu-schedule-diff',
